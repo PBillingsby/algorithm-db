919baea03a838ed747bddddd641a072a2c0b58fb
PBillingsby <philbillingsby@gmail.com>
2021-01-31 12:56:40 -0800

    Add style to Algorithm component

--- app/javascript/components/Algorithm.js
+++ app/javascript/components/Algorithm.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 class Algorithm extends React.Component {
   render() {
+    const examples = [...this.props.examples];
     return (
       <div className="container">
         <h1>{this.props.algorithm.name}</h1>
@@ -11,17 +12,20 @@ class Algorithm extends React.Component {
         <p>
           <b>Best case:</b> {this.props.algorithm.best_case}
         </p>
-        {Object.entries(this.props.examples).map(eg => {
-          <div className="text-left">
-            <p>
-              <b>Language:</b> {eg.language}
-            </p>
-            <div className="p-4 d-inline-block position-absolute bg-light">
-              <code>
-                <pre>{eg.example}</pre>
-              </code>
+        <hr></hr>
+        {examples.map(eg => {
+          return (
+            <div className="text-left">
+              <p>
+                <b>Language:</b> {eg.language}
+              </p>
+              <div className="p-4 d-inline-block position-absolute bg-light">
+                <code>
+                  <pre>{eg.example}</pre>
+                </code>
+              </div>
             </div>
-          </div>;
+          );
         })}
       </div>
     );
