b4909c192b2a6af2af66c3901fda4ab1ec5244d1
PBillingsby <philbillingsby@gmail.com>
2021-02-01 13:25:17 -0800

    Add key to passing props

--- app/javascript/components/Algorithm.js
+++ app/javascript/components/Algorithm.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 class Algorithm extends React.Component {
   render() {
     const examples = [...this.props.examples];
+    const unique = [...new Set(examples)].map(eg => {
+      eg.language;
+    });
+    debugger;
     return (
       <div className="container">
         <h1>{this.props.algorithm.name}</h1>
@@ -13,20 +17,27 @@ class Algorithm extends React.Component {
           <b>Best case:</b> {this.props.algorithm.best_case}
         </p>
         <hr></hr>
-        {examples.map(eg => {
-          return (
-            <div className="text-left">
-              <p>
-                <b>Language:</b> {eg.language}
-              </p>
-              <div className="p-4 d-inline-block position-absolute bg-light">
-                <code>
-                  <pre>{eg.example}</pre>
-                </code>
+        <select>
+          {unique.map(lang => {
+            <option>{lang}</option>;
+          })}
+        </select>
+        <div className="row">
+          {examples.map(eg => {
+            return (
+              <div className="col-sm">
+                <p>
+                  <b>Language:</b> {eg.language}
+                </p>
+                <div className="m-4 d-inline-block float-left p-3 position-absolute bg-light border border-black">
+                  <code>
+                    <pre>{eg.example}</pre>
+                  </code>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     );
   }
