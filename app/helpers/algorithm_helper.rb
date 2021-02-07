0dec8953a38445479d260139870ccb122d568e8a
PBillingsby <philbillingsby@gmail.com>
2021-02-07 10:03:40 -0800

    Remove react-rails controller and random_algorithm method

--- app/helpers/algorithm_helper.rb
+++
@@ -1,5 +0,0 @@
-module AlgorithmHelper
-  def random_algorithm
-    @random = Algorithm.order(Arel.sql('RANDOM()')).first
-  end
-end
--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -1,4 +1,4 @@
 class Algorithm < ApplicationRecord
-  has_many :examples
+  has_many :examples, dependent: :nullify
   accepts_nested_attributes_for :examples
 end
