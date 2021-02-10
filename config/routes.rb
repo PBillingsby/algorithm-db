9b18ff5cf4b1fb0ad74e032192d9f5c6b033d472
PBillingsby <philbillingsby@gmail.com>
2021-02-09 18:04:18 -0800

    Remove unneeded nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   resources :algorithms, only: [:index, :show, :new, :create, :update] do
     resource :examples, only: [:create]
-    resource :articles, only: [:new, :create]
+    resource :articles, only: [:new]
   end
   resources :examples, only: [:destroy]
+  resources :articles, only: [:create]
   root 'algorithms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
