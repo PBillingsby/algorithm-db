c4e18e73dae5ea49539fde62a75479fa0fd7892a
PBillingsby <philbillingsby@gmail.com>
2021-02-27 09:34:00 -0800

    Configure devise to add username for signup and signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :algorithms
   has_many :articles, through: :algorithms
+  attr_writer :login
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  def login
+    @login || self.username || self.email
+  end
 
   def get_leaderboard_data
-    byebug
+    {
+      algorithms: self.algorithms.count,
+      articles: self.articles.count
+    }
   end
 end
